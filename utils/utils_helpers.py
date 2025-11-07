e891b29990d09747482d8eaf7c0df357cb3a00f7
garpil28 <storegarfield076@gmail.com>
2025-11-07 07:58:53 +0700

    Update utils_helpers.py

--- utils/utils_helpers.py
+++ utils/utils_helpers.py
@@ -1,4 +1,3 @@
-# utils/helpers.py
 import asyncio
 from pyrogram.errors import FloodWait
 
@@ -11,15 +10,3 @@ async def safe_send(client, chat_id, text=None, **kwargs):
     except Exception as e:
         print("safe_send error:", e)
         return None
-
-async def chunked_broadcast(client, chat_id_list, text, batch=20, delay=1):
-    """Send to chat_id_list in chunks; handles FloodWait."""
-    n = 0
-    for cid in chat_id_list:
-        try:
-            await safe_send(client, cid, text)
-        except Exception as e:
-            print("broadcast to", cid, "failed", e)
-        n += 1
-        if n % batch == 0:
-            await asyncio.sleep(delay)
